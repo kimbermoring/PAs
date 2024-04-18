e22c922ad6490bb6ceaca383400d965a5ff5bab4
kimbermoring <160034790+kimbermoring@users.noreply.github.com>
2024-04-18 10:00:45 -0400

    Add files via upload

---
+++ PA 6.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 18 09:30:27 2024
+
+@author: KLMoring
+"""
+
+from Queue_for_ALGO import Queue
+
+def TrainingList(): 
+    
+    training = Queue 
+    
+    trainings.enqueue(new_training)
+    
+    
+    
+    
+TrainingList()
+
+
+
+
+
+
+
+    
+    
+    
+    
